f340c44358f20aece7d0da156233ae92895f2ecb
Robert Frazier <robert.v.frazier@gmail.com>
2018-02-20 11:10:49 -0500

    Start to convert generic input form to YouTube API app

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
-function main(){
+
+const YouTubeUrl='https://www.googleapis.com/youtube/v3/search';
+
+function userInput(){
     $('#js-form').submit(event => {
         event.preventDefault();
         let userChoice=$('input#js-search-category').val();
         $('input#js-search-category').val('');
-        $('.js-resultsList').html(`<li>${userChoice}</li>`);
+        // $('.js-resultsList').html(`<li>${userChoice}</li>`); // Test that we got the user's choice.
     });
 }
 
-main();
+$(userInput);
