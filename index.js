a83b72bc3250ecf57b6157aecbe890b794b3b347
Robert Frazier <robert.v.frazier@gmail.com>
2018-02-21 08:39:39 -0500

    Meet minimum requirements for the app
    User can enter a search term, the app gets thumbnail images from
    the YouTube API, and they display in a decent-looking, responsive layout
    on the web page. Clicking on a thumbnail opens the video in a new tab.
    
    To do:
    Get the videos to play in a lightbox.
    Add a button for each thumbnail to get more from that channel.
    Add a buttons to get more results (using the previous and next
    page links from the JSON).

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
 'use strict';
 
-const YouTubeUrl='https://www.googleapis.com/youtube/v3/search';
+const youTubeUrl='https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromApi(searchTerm, callback){
+    let query={
+        part: 'snippet',
+        maxResults: '10',
+        key: 'AIzaSyD9p4OgF8DmaoYZYAQPLUIyhU7rgRJYVRo',
+        q: searchTerm
+    }
+    $.getJSON(youTubeUrl, query, callback);
+}
+
+function renderResult(result){
+    console.log(result);
+    return `
+    <div class='thumbBox'>
+        <figure class='thumb'>
+            <a href='https://www.youtube.com/watch?v=${result.id.videoId}' target='_blank'><img src='${result.snippet.thumbnails.medium.url}' /></a>
+            <figcaption class='caption'>${result.snippet.title}</figcaption>
+        </figure>
+    </div>
+    `;
+}
+
+function displayYouTubeData(data){
+    let results=data.items.map((item, index)=>renderResult(item));
+    $('.js-resultsList').html(results);
+}
 
 function userInput(){
     $('#js-form').submit(event => {
         event.preventDefault();
         let userChoice=$('input#js-search-category').val();
         $('input#js-search-category').val('');
-        // $('.js-resultsList').html(`<li>${userChoice}</li>`); // Test that we got the user's choice.
+        getDataFromApi(userChoice, displayYouTubeData);
     });
 }
 
